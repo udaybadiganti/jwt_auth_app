152a18cbfbaa7ad2b568e506fef5788fb3395b15
udaay7446 <udaay7446@gmail.com>
2021-10-19 00:05:37 +0530

    added put,delete to lisings

--- sample/listing/urls.py
+++ sample/listing/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 #from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVirifyView
 #from acc_app.views import RetriveUserView, RegisterView
 from listing.views import ManagingListView
+from listing.listing_views import index
+
 urlpatterns = [
     path('manage/', ManagingListView.as_view()),
+    path('login/', index, name = 'index'),
     
 ]
--- sample/listing/views.py
+++ sample/listing/views.py
@@ -15,6 +15,7 @@ class ManagingListView(APIView):
 			if not user.is_owner:
 				return Response({'error': 'User does not have permission to retrive'},status = status.HTTP_403_FORBIDDEN)
 
+			#retriving form api arguments params
 			slug  = request.query_params.get('slug')
 
 			if not slug:
@@ -38,38 +39,119 @@ class ManagingListView(APIView):
 		
 	def post(self, request):
 		try:
-			print("ok3")
 			user = request.user
 			if not user.is_owner:
 				return Response({'error': 'user does not have permission to post'}, status = status.HTTP_403_FORBIDDEN)
 
 			data = request.data
-			print("ok4")
+			print(data.keys())
 			category = data['category']
 			item_name = data['item_name']
 			slug = data['slug']
 			#created_date = data['created_date']
-			print("ok5")
 
 			if Listing.objects.filter(slug = slug).exists():
 				return Response({'error': 'Listing with this slug is already exists'}, status = status.HTTP_400_BAD_REQUEST)
-			print("ok1")
+			
 			Listing.objects.create(
 				owner = user.email,
 				category = category,
 				item_name = item_name,
 				slug = slug
 				)
-			print("ok2")
+			
 			return Response({'success': 'Items successfully created.'}, status = status.HTTP_201_CREATED)
 
 		except:
 			return Response({'error': 'Something went wrong when creating list..'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+	def retrive_values(self, data):
+
+		category = data['category']
+		item_name = data['item_name']
+		slug = data['slug']
+
+		data = {
+			'category': category,
+			'item_name': item_name,
+			'slug': slug
+		}
+		return data
+
 	def put(self, request):
+		try:
+			user = request.user
 
+			if not user.is_owner:
+				return Response({'error': 'User does not have permission to create items.'}, status = status.HTTP_400_BAD_REQUEST)
+
+			data = request.data
+
+
+			data = self.retrive_values(data)
+
+			category = data['category']
+			item_name = data['item_name']
+			slug = data['slug']
+
+			if not Listing.objects.filter(slug = slug).exists():
+				return Response({'error': 'Listing with this slug is already exists'}, status = status.HTTP_400_BAD_REQUEST)
+
+			Listing.objects.create(
+				owner = user.email,
+				category = category,
+				item_name = item_name,
+				slug = slug
+				)
+			return Response({'success': 'Items successfully created.'}, status = status.HTTP_201_CREATED)
+
+
+		except:
+			return Response({'error': 'Something went wrong when creating list..'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+	def delete(self, request):
+		try:
+			user = request.user
+
+			if not user.is_owner:
+				return Response({'error': 'User does not have permission to create items.'}, status = status.HTTP_400_BAD_REQUEST)
+
+			slug = request.query_params.get('slug')
+			#print(slug)
+
+			if not Listing.objects.filter(owner = user.email, slug = slug).exists():
+				return Response({'error': 'Listing slug is not available'}, status = status.HTTP_400_BAD_REQUEST)
+
+			Listing.objects.filter(owner=user.email, slug=slug).delete()
+
+			if not Listing.objects.filter(owner=user.email, slug=slug).exists():
+				return Response(status=status.HTTP_204_NO_CONTENT)
+			else:
+				return Response({'error': 'Failed to delete listing'},status=status.HTTP_400_BAD_REQUEST)
+			
+
+
+		except:
+			return Response({'error': 'Something went wrong when creating list..'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+'''
+	def retrive_values_patch(self, data):
 		pass
-
 
 	def patch(self, request):
-		pass
+		try:
+			user = request.user
+
+			if not user.is_owner:
+				return Response({'error': 'User does not have permission to create items.'}, status = status.HTTP_400_BAD_REQUEST)
+
+			data = request.data
+
+			data = self.retrive_values_patch(data)
+
+			if slug	
+
+		except:
+			return Response({'error': 'Something went wrong when creating list..'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+'''
